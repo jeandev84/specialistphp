4c643d077b0a7ce0d8f6a72a83c987f8af1d0f22
Жан-Клод <jeanyao@ymail.com>
2023-04-18 19:08:04 +0300

    adapte template to bootstrap

--- PHPLevel1/mysite.local/index.php
+++ PHPLevel1/mysite.local/index.php
@@ -38,14 +38,15 @@ switch($page) {
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<div id="header">
-    <!-- Верхняя часть страницы -->
-    <!-- <img src="img/specialist.jpg" width="187" height="29" alt="Наш логотип" class="logo" /> -->
-    <img src="img/specialist.jpg" width="150" alt="Наш логотип" class="logo" />
-    <span class="slogan">приходите к нам учиться</span>
-    <!-- Верхняя часть страницы -->
-</div>
-
+<nav>
+    <div class="container">
+        <!-- Верхняя часть страницы -->
+        <!-- <img src="img/specialist.jpg" width="187" height="29" alt="Наш логотип" class="logo" /> -->
+        <img src="img/specialist.jpg" width="150" alt="Наш логотип" class="logo" />
+        <span class="slogan">приходите к нам учиться</span>
+        <!-- Верхняя часть страницы -->
+    </div>
+</nav>
 <div class="container">
 
     <div class="row">
