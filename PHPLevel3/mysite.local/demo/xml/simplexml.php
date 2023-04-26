0083ad226e926e7758b5ef95181d43720c50b8dc
Жан-Клод <jeanyao@ymail.com>
2023-04-26 23:14:29 +0300

    SIMPLE XML

--- PHPLevel3/mysite.local/demo/xml/simplexml.php
+++ PHPLevel3/mysite.local/demo/xml/simplexml.php
@@ -1,26 +1,32 @@
 <?php 
 header( "Content-Type: text/html;charset=utf-8");
 
-// Загружаем документ и преобразуем его в объект
-$sxml = simplexml_load_file("catalog.xml");
+// SimpleXML для добавления не используется. А для Изменения данных удобный
+// Загружаем, документ и преобразуем его в объект
+$simplexml = simplexml_load_file("catalog.xml");
 
+
+
+/*
 // Загружаем XML-строку и преобразуем его в объект
-$sxml = simplexml_load_string("XML строка");
+$simplexml = simplexml_load_string("XML строка");
 
 // Получение текста нужного элемента (название второй книги)
-echo $sxml->book[1]->title;
+echo $simplexml->book[1]->title;
 
 // Получение атрибута элемента
-echo $sxml->book[1]->title["lang"];
+echo $simplexml->book[1]->title["lang"];
 
 // Изменение текста нужного элемента (название первой книги)
-$sxml->book[0]->title = "Новое название";
+$simplexml->book[0]->title = "Новое название";
 
 // Преобразование объекта в строку
-$xml = $sxml->asXML();
+$xml = $simplexml->asXML();
 
 // Запись строки в файл
 file_put_contents("catalog.xml", $xml);
+*/
+
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -38,7 +44,15 @@ file_put_contents("catalog.xml", $xml);
       <th>Цена, руб</th>
     </tr>
     <?php 
-      //Парсинг 
+      //Парсинг
+      foreach ($simplexml->book as $book) {
+          echo "<tr>";
+          echo "<td>{$book->author}</td>";
+          echo "<td>{$book->title}</td>";
+          echo "<td>{$book->pubyear}</td>";
+          echo "<td>{$book->price}</td>";
+          echo "<tr>";
+      }
     ?>
   </table>
 </body>
