69dc73f41c66412474bd4e861e1f501a8ed6aa4a
Жан-Клод <jeanyao@ymail.com>
2023-04-26 23:05:54 +0300

    CREATE RSS Новостная Lenta

--- PHPLevel3/mysite.local/demo/xml/simplexml.php
+++ PHPLevel3/mysite.local/demo/xml/simplexml.php
@@ -1,10 +1,31 @@
 <?php 
-  header( "Content-Type: text/html;charset=utf-8"); 
-?>
-<html>
+header( "Content-Type: text/html;charset=utf-8");
 
+// Загружаем документ и преобразуем его в объект
+$sxml = simplexml_load_file("catalog.xml");
+
+// Загружаем XML-строку и преобразуем его в объект
+$sxml = simplexml_load_string("XML строка");
+
+// Получение текста нужного элемента (название второй книги)
+echo $sxml->book[1]->title;
+
+// Получение атрибута элемента
+echo $sxml->book[1]->title["lang"];
+
+// Изменение текста нужного элемента (название первой книги)
+$sxml->book[0]->title = "Новое название";
+
+// Преобразование объекта в строку
+$xml = $sxml->asXML();
+
+// Запись строки в файл
+file_put_contents("catalog.xml", $xml);
+?>
+<!DOCTYPE html>
+<html lang="ru">
 <head>
-  <title>Каталог</title>
+    <title>Каталог</title>
 </head>
 
 <body>
