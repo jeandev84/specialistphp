4086e6653b10495083c189262079a08d819369be
Жан-Клод <jeanyao@ymail.com>
2023-04-28 07:17:53 +0300

    InfiniteIterator

--- PHPLevel4/mysite.local/demo/spl/10-limitIteratorExample.php
+++ PHPLevel4/mysite.local/demo/spl/10-limitIteratorExample.php
@@ -1,5 +1,44 @@
 <?php
 
+// Infinite Iterator
+interface TaskInterface {
+   public function getAction();
+}
+
+class Task implements TaskInterface
+{
+     private $arguments;
+
+
+     /**
+      * @param ...$args
+     */
+     public function __construct(...$args)
+     {
+         $this->arguments = $args;
+     }
+
+     public function getAction()
+     {
+         return $this->arguments;
+     }
+}
+
+$t1 = new Task(3);
+$t2 = new Task([4, 5, 6, 7]);
+$t3 = new Task([9, 8, 7]);
+
+$arrayIterator    = new ArrayIterator([$t1, $t2, $t3]);
+$infiniteIterator = new InfiniteIterator($arrayIterator);
+
+foreach ($infiniteIterator as $task) {
+    $stop = $task->getAction();
+    if ($stop) break;
+    usleep(200);
+}
+
+
+// LimitIterator
 // Данные из базы
 $data = [
     ['name' => 'John'],
